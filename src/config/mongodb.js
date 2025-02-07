3cbdfc08c74129e079c3dd80230fc624b54c454f
minhphu102003 <minhphu01200@gmail.com>
2025-02-07 23:45:07 +0700

    add mongo config file

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,2 +1,25 @@
-/**
- */
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGOBD_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+    if (!trelloDatabaseInstance) {
+        throw new Error('Call CONNECT_DB first')
+    }
+
+    return trelloDatabaseInstance
+}
